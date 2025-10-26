e0b07ae422c5e9ca24b8981041c42897cefa5829
Haaddy <H00DHaaddy@gmail.com>
2025-10-26 15:35:16 +0300

    first commit

---
+++ TwitchTraker/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TwitchTraker;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
